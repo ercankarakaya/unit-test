674e4f02f3489c99c5683135ea7bf02115642d98
Ercan Karakaya <karakayaercan71@gmail.com>
2023-06-28 20:41:56 +0300

    Lombok Test

--- UnitTest-Example/src/test/java/assertj/LombokTest.java
+++ UnitTest-Example/src/test/java/assertj/LombokTest.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 public class LombokTest {
 
     @Test
-    public void testLombok() throws Exception{
-       // Car car = new Car("Tesla");
+    public void testLombok() throws Exception {
+        // Car car = new Car("Tesla");
+        // car.setName("Tesla");
         Car car = Car.builder().name("Tesla").build();
         System.out.println(car.toString());
     }
