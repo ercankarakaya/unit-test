4d5e96b6f6a5721857d807c540ff5d8224f3d153
Ercan Karakaya <karakayaercan71@gmail.com>
2023-07-08 14:23:08 +0300

    PersonDto created.

---
+++ springboot-junit5-test/src/main/java/com/ercan/dto/PersonDto.java
@@ -0,0 +1,23 @@
+package com.ercan.dto;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of={"id"})
+@Builder
+public class PersonDto {
+
+    private Long id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    private String gender;
+    private String email;
+    private List<String> addresses;
+}
--- springboot-junit5-test/src/main/java/com/ercan/entity/Person.java
+++ springboot-junit5-test/src/main/java/com/ercan/entity/Person.java
@@ -15,7 +15,8 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
 
     @Enumerated(EnumType.STRING)
