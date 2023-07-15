0d17bcf16f151df94a3321991bf2d454ae01705b
Ercan Karakaya <karakayaercan71@gmail.com>
2023-07-15 20:41:00 +0300

    JUnitExampleTest

---
+++ springboot-junit5-test-2/src/test/java/JUnitExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnitExampleTest {
+
+    @Test
+    void testMethod(){
+        assertTrue(true);
+    }
+}
