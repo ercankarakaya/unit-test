aaeeb8abc0ed67d593dae860c1c3f68550c95dab
Ercan Karakaya <karakayaercan71@gmail.com>
2023-07-15 20:03:26 +0300

    initial commit

---
+++ springboot-junit5-test-2/src/main/java/com/ercan/SpringBootJUnit5Application.java
@@ -0,0 +1,12 @@
+package com.ercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJUnit5Application.class, args);
+    }
+}
