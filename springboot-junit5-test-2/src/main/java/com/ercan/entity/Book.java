3c8baac570b7160e5d488cdc44d1d409627d18ca
Ercan Karakaya <karakayaercan71@gmail.com>
2023-07-15 20:42:57 +0300

    Created Book Entity and Database connection

---
+++ springboot-junit5-test-2/src/main/java/com/ercan/entity/Book.java
@@ -0,0 +1,30 @@
+package com.ercan.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String summary;
+
+    private int rating;
+}
